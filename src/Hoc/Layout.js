cf298b226110a51e2d162106ca89e215daaecfed
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-02-09 11:05:58 +0100

    Done with initial setup, header and footer

---
+++ src/Hoc/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header_Footer/Header'
+import Footer from '../components/Header_Footer/Footer'
+
+
+export default function Layout(props) {
+  return (
+    <div>
+      <Header/>
+      {props.children}
+      <Footer/>
+    </div>
+  )
+}
---
+++ src/components/Header_Footer/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
+import {CityLogo} from '../UI/Icons'
+
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <AppBar position="fixed" style={{ backgroundColor: '#98c5e9', boxShadow: 'none', padding: '10px 0', borderBottom: '2px solid #00285e' }}>
+        <Toolbar style={{ display: 'flex' }}>
+          <div style={{ flexGrow: 1 }}>
+            <div className="header_logo">
+              <CityLogo linkTo="/" link={true} width="70px" height="70px"/>
+            </div>
+          </div>
+          <Link to="teams">
+            <Button color="inherit" >The Team</Button>
+          </Link>
+          
+          <Link to="matches">
+            <Button  color="inherit">The Matches</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
