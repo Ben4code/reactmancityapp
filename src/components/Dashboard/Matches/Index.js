b490a058d3755daf92968ea8cc52aa93d7d0db1d
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-05-06 22:24:19 +0100

    Getting ready to deploy

--- src/components/Dashboard/Matches/Index.js
+++ src/components/Dashboard/Matches/Index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import AdminLayout from '../../../Hoc/AdminLayout'
+// import {Link} from 'react-router-dom'
+// import AdminLayout from '../../../Hoc/AdminLayout'
 
 // Table Imports
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// import Paper from '@material-ui/core/Paper';
 
 
 export default class Index extends Component {
